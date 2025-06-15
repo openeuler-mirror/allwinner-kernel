26537b5b4016f3acbcbacb46982826c3f35d15cc
Hsun Lai <i@chainsx.cn>
2025-06-16 01:33:43 +0800

    cpufreq-dt-platdev-Blocklist-allwinner-h618-SoC

--- drivers/cpufreq/cpufreq-dt-platdev.c
+++ drivers/cpufreq/cpufreq-dt-platdev.c
@@ -105,6 +105,7 @@ static const struct of_device_id allowlist[] __initconst = {
 static const struct of_device_id blocklist[] __initconst = {
 	{ .compatible = "allwinner,sun50i-h6", },
 	{ .compatible = "allwinner,sun50i-h616", },
+	{ .compatible = "allwinner,sun50i-h618", },
 
 	{ .compatible = "apple,arm-platform", },
 
